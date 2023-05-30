adb7b6b8c1c33f5d0ddec97e118b1bf1f74ab1f2
haamedshadow <133811601+haamedshadow@users.noreply.github.com>
2023-05-30 16:43:37 +0000

    new commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link"
-
 type Props = {}
 
 export default function Home({}: Props) {
-  return <div>page
-    <button><Link href="/auth/login">login</Link></button>
-  </div>
+  return <div>page</div>
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require('@material-tailwind/react/utils/withMT');
+const withMT = require("@material-tailwind/react/utils/withMT")
+
 module.exports = withMT({
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    'path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-    'path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // config for mono repo
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      animation: {
-        rgb: 'rgb 10s linear infinite',
-      },
-      colors: {
-        rgb: 'rgba(105, 105, 111, 0.25)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-});
+})
