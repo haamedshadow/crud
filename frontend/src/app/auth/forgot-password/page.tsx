37032848b9a0d76222f9ee40f7fe0fbfc6e91250
haamedshadow <133811601+haamedshadow@users.noreply.github.com>
2023-05-30 16:56:29 +0000

    new commit

--- frontend/src/app/auth/forgot-password/page.tsx
+++ frontend/src/app/auth/forgot-password/page.tsx
@@ -1,5 +1,5 @@
-type Props = {};
+type Props = {}
 
 export default function forgotpassword({}: Props) {
-  return <div>forgotpassword</div>;
+  return <div>forgotpassword</div>
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link"
+
 type Props = {}
 
 export default function Home({}: Props) {
-  return <div>page</div>
+  return (
+    <div>
+      <Link href="/auth/login">Login</Link> |{" "}
+      <Link href="/auth/register">Register</Link> |{" "}
+      <Link href="/auth/forgot-password">Forgot Password</Link> |{" "}
+      <Link href="/auth/reset-password">Reset Password</Link> |{" "}
+      <Link href="/auth/change-password">Change Password</Link> |{" "}
+      <Link href="/auth/verify-email">Verify Email</Link> |{" "}
+      <Link href="/auth/update-profile">Update Profile</Link> |{" "}
+      <Link href="/auth/logout">Logout</Link>
+    </div>
+  )
 }
